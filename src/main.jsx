2cc67827ef3569e758b9cbe3f6e2e936d5f302cc
CandyyPL <aplay58.yt@gmail.com>
2022-08-31 10:46:37 +0200

    Added webfontloader

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from '@/assets/styles/GlobalStyle'
 import App from '@/pages/App'
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: ['Nunito'],
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

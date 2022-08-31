72ef0a520f2a37154253677aec2bc68c344e6edb
CandyyPL <aplay58.yt@gmail.com>
2022-08-31 10:43:54 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GlobalStyle from '@/assets/styles/GlobalStyle'
+import App from '@/pages/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+)
